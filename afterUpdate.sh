bece3203eaa42f1c8b86b1177e2727c2e0906b04
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2022-08-01 16:08:09 -0500

    Update afterUpdate.sh

--- afterUpdate.sh
+++ afterUpdate.sh
@@ -4,6 +4,6 @@ if [ -f PAUmain.sh]; then
   rm PAUmain.sh
 fi
 
-https://github.com/brendenhoffman/PVEAfterUpdate/blob/main/PAUmain.sh
+wget https://github.com/brendenhoffman/PVEAfterUpdate/blob/main/PAUmain.sh
 
 source PAUmain.sh
