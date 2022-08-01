4ac5dd95c92069c48ae5316091b623ef4e6fcf26
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2022-08-01 14:50:51 -0500

    Create afterUpdate.sh

---
+++ afterUpdate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -f PVEDiscordDark.sh ]; then
+  rm PVEDiscordDark.sh
+fi
+
+wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh
+source PVEDiscordDark.sh install
+
+echo "Removing \"No Subscription\" pop-up"
+sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+
+echo "Restarting pveproxy.service. This may take a minute."
+systemctl restart pveproxy.service
