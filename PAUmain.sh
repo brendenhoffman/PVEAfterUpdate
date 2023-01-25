d412c41ac841e1f6761e6f2b135ce5e2f7e3f32e
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2023-01-25 10:36:36 -0600

    Update for 7.3-4

--- PAUmain.sh
+++ PAUmain.sh
@@ -9,7 +9,8 @@ wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscord
 bash PVEDiscordDark.sh install
 
 echo "Removing \"No Subscription\" pop-up"
-sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+#As of 7.3-4, the best way I found to remove the popup is to remove the function altogether
+sed -i.bak 's/if (false) { Ext.Msg.show({ title: gettext('\''No valid subscription'\''),/if (false) { void({ \/\/Ext.Msg.show({ title: gettext('\''No valid subscription'\''),/g' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 
 echo "Restarting pveproxy.service. This may take a minute."
 systemctl restart pveproxy.service
