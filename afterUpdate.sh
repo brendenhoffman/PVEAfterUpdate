007587c1327653aa253910fec5c20711077dd59d
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2023-02-24 21:27:27 -0600

    Update afterUpdate.sh

--- afterUpdate.sh
+++ afterUpdate.sh
@@ -2,4 +2,5 @@
 
 [ -f PAUmain.sh ] && echo "Removing PAUmain.sh" && rm PAUmain.sh
 
+echo "Running after-update tasks"
 curl -L https://raw.githubusercontent.com/brendenhoffman/PVEAfterUpdate/master/PAUmain.sh | sh
